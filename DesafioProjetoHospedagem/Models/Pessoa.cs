4f24ce34d34398940e1bd0b818ea0580a617ccb4
vladimirca2000 <vladimirca2000@gmail.com>
2025-01-15 16:30:21 -0300

    Resolução do desafio hospedagem

---
+++ DesafioProjetoHospedagem/Models/Pessoa.cs
@@ -0,0 +1,26 @@
+﻿namespace DesafioProjetoHospedagem.Models;
+
+public class Pessoa
+{
+    public Pessoa()
+    {
+        Nome = string.Empty; // Initialize Nome to avoid nullability issues
+        Sobrenome = string.Empty; // Initialize Sobrenome to avoid nullability issues
+    }
+
+    public Pessoa(string nome)
+    {
+        Nome = nome;
+        Sobrenome = string.Empty; // Initialize Sobrenome to avoid nullability issues
+    }
+
+    public Pessoa(string nome, string sobrenome)
+    {
+        Nome = nome;
+        Sobrenome = sobrenome;
+    }
+
+    public string Nome { get; set; }
+    public string Sobrenome { get; set; }
+    public string NomeCompleto => $"{Nome} {Sobrenome}".ToUpper();
+}
---
+++ DesafioProjetoHospedagem/Models/Suite.cs
@@ -0,0 +1,20 @@
+﻿namespace DesafioProjetoHospedagem.Models;
+
+public class Suite
+{
+    public Suite()
+    {
+        TipoSuite = string.Empty;
+    }
+
+    public Suite(string tipoSuite, int capacidade, decimal valorDiaria)
+    {
+        TipoSuite = tipoSuite;
+        Capacidade = capacidade;
+        ValorDiaria = valorDiaria;
+    }
+
+    public string TipoSuite { get; set; }
+    public int Capacidade { get; set; }
+    public decimal ValorDiaria { get; set; }
+}
